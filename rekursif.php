f0db8429f9edeeff69254f6aadd52a574b599bd7
frznst <42663055+frznst@users.noreply.github.com>
2018-09-13 08:58:12 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,48 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif($i, $batas=0){
+ if($batas<$i){
+ 	echo "*";
+ 	$batas++;
+ 	rekursif($i,$batas);
+ }
+ else{
+ 	echo "<br>";
+ 	$i--;
+ 	$batas=0;
+ 	rekursif($i,$batas);
+ }
+
 }
 rekursif(5);
 ?>
+/*====================================================================================*/
+<?php
+
+function rekursif($i, $batas=1, $count=1){
+if($i>$batas){
+	$i--;
+	$count++;
+	rekursif($i,$batas,$count);
+}
+else{
+	if($i<=$batas&&$i>0){
+		echo "$i";
+		$i--;
+		rekursif($i);
+	}	
+	
+	else {
+		echo "<br>";
+		$batas++;
+		$i=$count;
+
+		rekursif($i,$batas);
+	}
+	}
+}
+
+
+rekursif(5);
+?>
+
